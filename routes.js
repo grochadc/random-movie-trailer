8bbc532d7df89846dc4fd552e66d469b90acf44b
Gonzalo Rocha <grochadc@gmail.com>
2017-10-04 17:56:43 -0500

    Change the randomExc function to its own file. And rename it random.exclude. Now it has to be required

--- routes.js
+++ routes.js
@@ -13,27 +13,11 @@ var Xray = require('x-ray');
 var x = Xray();
 
 var debug = false;
+var random = require('./lib/random');
 
-
-//Generate a random number excluding the numbers on the given array
-function randomExc(min, max, arr) {
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    if(arr){
-      var exclude = arr.every(function(val){ return arr.indexOf(num) >= 0 });
-      if(exclude){
-        console.log('Random num is in array, do over');
-        randomExc(min, max, arr);
-      }
-      else{
-        console.log('Random num is not in array ',num);
-        return num;
-      }
-    }
-    else {
-      return num;
-    }
-};
+//=============================//
+//    R O U T E S             //
+//===========================//
 
 router.get('/', (req, res) => {
   console.log('Page requested!');
@@ -82,12 +66,12 @@ router.get('/', (req, res) => {
         if(req.cookies.randomInt) {
           var cookieStr = req.cookies.randomInt;
           var cookieArr = typeof(cookieStr) == 'string' ? JSON.parse(cookieStr) : cookieStr; //Make sure the cookie is an Array
-          randomInt = randomExc(0,movies.length,cookieArr);
+          randomInt = random.exclude(0,movies.length,cookieArr, true);
           cookieArr.push(randomInt);
           res.cookie('randomInt',cookieArr);
         }
         else{
-          randomInt = randomExc(0,movies.length)
+          randomInt = random.exclude(0,movies.length, true)
         res.cookie('randomInt', JSON.stringify(randomInt));
         }
 
