daba4d57c1d293e3c7738f01710a95d875881cc3
Gonzalo Rocha <grochadc@gmail.com>
2017-10-11 10:41:35 -0500

    Setup Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+          files: grunt.file.expandMapping(['**/*.js', '*.js'],'dist/', {
+            cwd: 'src/'
+          })
+      }
+    },
+    copy: {
+      files: {expand: true, cwd: 'src/', src: ['models/*','public/*', 'views/**/*', 'vies/*'], dest: 'dist/'}
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['uglify','copy']);
+
+};
