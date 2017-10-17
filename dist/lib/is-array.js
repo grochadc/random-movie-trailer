6c4e7bea5e115555298c1ab98a1f0e0858700d1f
Gonzalo Rocha <grochadc@gmail.com>
2017-10-17 12:07:06 -0500

    Add more movies to model

--- dist/lib/is-array.js
+++ dist/lib/is-array.js
@@ -1,3 +1,3 @@
-/*! random-movie-trailer 2017-10-16 */
+/*! random-movie-trailer 2017-10-17 */
 
 module.exports=function(t){const o=Object.prototype.toString.call(t);return"[object Array]"==o||"[object Object]"!=o&&void 0};
