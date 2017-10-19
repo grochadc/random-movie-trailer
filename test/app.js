20aca15a4ae222b7dd169dd05a15d279692a5b43
Gonzalo Rocha <grochadc@gmail.com>
2017-10-18 19:29:44 -0500

    Fix the app tet  returning res = undefined

--- test/app.js
+++ test/app.js
@@ -22,6 +22,22 @@ describe('##App ' + (env=='dev' ? '(development)' : '(production)'), () => {
           res.should.have.status(200);
           done();
         });
+
     });
+
   });
+
+  describe('Index:', () =>{
+     it('index=0 should send a 200 response', (done) => {
+       chai.request(app)
+         .get('/')
+         .query({index: 0})
+         .end((err, res) => {
+           should.exist(res);
+           res.should.have.status(200);
+           done();
+         });
+     });
+   });
+
 });
