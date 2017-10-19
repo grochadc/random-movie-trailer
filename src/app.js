a236c19b909560b7dcbc4d94da67e06e70dbe348
Gonzalo Rocha <grochadc@gmail.com>
2017-10-18 23:11:43 -0500

    Clear the code

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
-const compression = require('compression');
 const express = require('express');
 
 //Define app and settings
 const app = express();
 const exphbs = require('express-handlebars');
 var cookieParser = require('cookie-parser');
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 3000;
-
-var path = require('path');
+const compression = require('compression');
+const path = require('path');
 
 var routes = require(path.join(__dirname, 'controllers/routes'));
 
-var debug = process.env.NODE_ENV == 'test' ? false : true;
-
-app.use(compression());
+/**
+Settings
+**/
+const port = process.env.PORT || 3000; // process.env.PORT lets the port be set by Heroku
+const debug = process.env.NODE_ENV == 'test' ? false : true;
 
 const publicFolder = path.join(__dirname, 'public/');
 app.use(express.static(publicFolder));
+
+app.use(compression());
 app.use(cookieParser());
 app.use('/', routes);
-
-//app.use(cookieParser());
 
 //View engine
 const viewsPath = path.join(__dirname,'views/');
@@ -36,7 +33,6 @@ app.set('view engine', 'handlebars');
 
 app.listen(port, function () {
   if(debug) console.log('Server Starts on '+port);
-  //if(!debug) logs.disable(); //Disable logging if debug variable is false
 });
 
 module.exports = app; //For testing with Mocha
